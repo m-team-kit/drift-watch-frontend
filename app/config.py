2fcc397ec10cbfb38c0c10e5afb31a9d25e5270c
Borja Esteban <boressan@outlook.com>
2024-07-15 16:01:03 +0200

    fix routes for reverse proxy, dev and format utils

--- app/config.py
+++ app/config.py
@@ -10,16 +10,20 @@ class Settings(BaseSettings):
         case_sensitive=False,
     )
 
-    DRIFT_MONITOR_HOST: str = "localhost"
     DRIFT_MONITOR_SCHEME: str = "http"
+    DRIFT_MONITOR_HOST: str = "localhost"
     DRIFT_MONITOR_PORT: int = 5000
+    DRIFT_MONITOR_VERSION: str = ""
     DRIFT_MONITOR_TIMEOUT: int = 10
 
 
 # Initialize the settings object
 settings = Settings()
 monitoring_url = (
-    f"{settings.DRIFT_MONITOR_SCHEME}://"
-    f"{settings.DRIFT_MONITOR_HOST}:"
-    f"{settings.DRIFT_MONITOR_PORT}"
+    f"{settings.DRIFT_MONITOR_SCHEME}:"
+    f"//{settings.DRIFT_MONITOR_HOST}"
+    f":{settings.DRIFT_MONITOR_PORT}"
 )
+if settings.DRIFT_MONITOR_VERSION:
+    api_prefix = f"/api/{settings.DRIFT_MONITOR_VERSION}"
+    monitoring_url += api_prefix
--- app/utils.py
+++ app/utils.py
@@ -8,6 +8,7 @@ from app.config import monitoring_url, settings
 import base64
 import json
 
+
 def fetch_uncompleted(start_datetime, end_datetime):
     """Fetch all uncompleted jobs based on the selected date and time."""
     query_list = [
@@ -20,9 +21,6 @@ def fetch_uncompleted(start_datetime, end_datetime):
         timeout=settings.DRIFT_MONITOR_TIMEOUT,
     )
     response.raise_for_status()
-    
-    
-    
     return [
         {
             "Experiment_id": job.get("id", "Missing ID"),
@@ -47,44 +45,47 @@ def fetch_drifts(start_datetime, end_datetime, data=True, concept=True):
         timeout=settings.DRIFT_MONITOR_TIMEOUT,
     )
     response.raise_for_status()
-    
 
     json_data = json.loads(response.text)
- 
-    data_array = []    
+
+    data_array = []
     for item in json_data:
         data_drift_parameters = item["data_drift"]["parameters"]
 
         concept_drift_parameters = item["concept_drift"]["parameters"]
-        
+
         # if image exists, extract the encoded image data from the JSON response
-        if item['data_drift']['parameters'].get('MMD_statistic_image', None):
-            encoded_MMD_statistic_image = item['data_drift']['parameters']['MMD_statistic_image']
+        if item["data_drift"]["parameters"].get("MMD_statistic_image", None):
+            encoded_MMD_statistic_image = item["data_drift"]["parameters"][
+                "MMD_statistic_image"
+            ]
             # Decode the base64-encoded image data to binary
             MMD_statistic_image = base64.b64decode(encoded_MMD_statistic_image)
 
         else:
             MMD_statistic_image = None
-        
 
         # Extract the encoded image data from the JSON response
-        if item['data_drift']['parameters'].get('data_distribution_image', None):
-            encoded_data_distribution_image = item['data_drift']['parameters']['data_distribution_image']
+        if item["data_drift"]["parameters"].get("data_distribution_image", None):
+            encoded_data_distribution_image = item["data_drift"]["parameters"][
+                "data_distribution_image"
+            ]
             # Decode the base64-encoded image data to binary
             data_distribution_image = base64.b64decode(encoded_data_distribution_image)
         else:
             data_distribution_image = None
 
-        data_array.append({
-            "drift_run_id": item["id"],
-            "Experiment_time": item["datetime"],
-            "data_drift": item["data_drift"]["drift"],
-            "concept_drift": item["concept_drift"]["drift"],
-            "concept_drift_parameters" : concept_drift_parameters,
-            "data_drift_parameters": data_drift_parameters,            
-            "MMD_statistic_image" : MMD_statistic_image,
-            "data_distribution_image": data_distribution_image
-    })
-
+        data_array.append(
+            {
+                "drift_run_id": item["id"],
+                "Experiment_time": item["datetime"],
+                "data_drift": item["data_drift"]["drift"],
+                "concept_drift": item["concept_drift"]["drift"],
+                "concept_drift_parameters": concept_drift_parameters,
+                "data_drift_parameters": data_drift_parameters,
+                "MMD_statistic_image": MMD_statistic_image,
+                "data_distribution_image": data_distribution_image,
+            }
+        )
 
     return data_array
